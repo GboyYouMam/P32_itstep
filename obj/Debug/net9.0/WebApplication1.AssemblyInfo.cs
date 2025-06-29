a19b08004ddac080b21764a03b5be4c11f9add86
GboyYouMam <kozahar185@gmail.com>
2025-06-29 12:37:22 +0300

    name of folder Note in Views changed to Notes

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c624368f286249668bcff798def1396ae8330949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfcfe4ed12025b2eb05c92504b34b35c1eed282")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
