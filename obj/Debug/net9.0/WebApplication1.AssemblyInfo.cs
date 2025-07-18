0f0f6abc8814e6efdf9b3230d3e94464f9c033bf
GboyYouMam <kozahar185@gmail.com>
2025-07-18 23:59:59 +0300

    fixed, now it running

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed30e49b8e144f571fbc1e340b401537d573d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a345b4b913210611efd71aa2d31adb788df6a67f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
